5290f0fccee4f6009ac639d82ceb40e15b8fefec
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-03-18 20:05:17 +0530

    TazaKhabar

---
+++ src/components/NewsItem.js
@@ -0,0 +1,77 @@
+// import React, { Component } from 'react'
+
+// export class NewsItem extends Component {
+  
+
+//   render() {
+//     let  {title , description, imageurl, newsurl} = this.props;
+//     return (
+//       <div className='my-3'>
+        
+//             <div className="card" style={{width: "18rem"}}>
+//             <img src={!imageurl?"https://images-na.ssl-images-amazon.com/images/I/511wU0wVpEL.jpg": imageurl} className="card-img-top" alt="..." />
+//             <div className="card-body">
+//               <h5 className="card-title">{title}</h5>
+//               <p className="card-text">{description}</p>
+//               <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+//             </div>
+//           </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default NewsItem
+
+// import React, { Component } from 'react'
+
+// export class NewsItem extends Component {
+//     render() {
+//         let {title, description, imageUrl, newsUrl} = this.props;
+//         return (
+//             <div className="my-3">
+//                 <div className="card">
+//                     <img src={!imageUrl?"https://images-na.ssl-images-amazon.com/images/I/511wU0wVpEL.jpg":imageUrl} className="card-img-top" alt="..."/>
+//                     <div className="card-body">
+//                         <h5 className="card-title">{title}</h5>
+//                         <p className="card-text">{description}</p>
+//                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default NewsItem
+
+import React from 'react'
+
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
+}
+
+export default NewsItem
